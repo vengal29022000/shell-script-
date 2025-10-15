88a4305f59a1db3413d51281842c0dac73400cf3
gowthamflm <vengalareddy.challa@gmail.com>
2025-10-15 08:15:12 +0530

    all

---
+++ 01-install.sh
@@ -0,0 +1,24 @@
+ID=(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo "please run the script with the user"
+    exit 1
+else 
+    echo "you are a root user"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+        echo "installation of mysql got failed"
+    else 
+        echo "installation of mysql got success"
+    fi
+else
+    echo "mysql got installed already"
+fi            
