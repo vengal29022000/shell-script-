a288ba4eb090e140bff4ef60da5afdae2ee3cff9
gowthamflm <vengalareddy.challa@gmail.com>
2025-10-16 10:02:40 +0530

    commit1

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 
 USERID=$(id -u)
-R="/e[31m"
-G="/e[32m"
-Y="/e[33m"
-N="/e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-SOURCE_DIR="/home/ec2-user/app-log"
+SOURCE_DIR="/home/ec2-user/app-logs"
 
-LOGS_FOLDER="/var/log/shell-script.logs"
-FILE_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%M-%d-%H-%M-%S)
-
-LOG_FILE_NAME="$LOG_FOLDER/$FILE_NAME-$TIMESTAMP.log"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 .... $R FAILURE $N"
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2 .... $G success $N"
-    fi    
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
-if [ $USERID -ne 0 ]
-then 
-    echo "ERROR:: YOU MUST RUN THIS SCRIPT WITH ROOT USER"
-    exit 1
-fi   
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
+}
 
-echo "script started executing at $TIMESTAMP" &>>$LOG_FILE_NAME
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Files to be deleted: $FILES_TO_DELETE"
 
-echo "files need to be deleted:: $FILES_TO_DELETE"
-
-while read -r filepath
+while read -r filepath # here filepath is the variable name, you can give any name
 do
-  echo "files to dlt $filepath"
-  rm -rf $filepath
-  echo "deleted files $filepath"
-done << $FILES_TO_DELETE
+    echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
+    rm -rf $filepath
+    echo "Deleted file: $filepath"
+done <<< $FILES_TO_DELETE
